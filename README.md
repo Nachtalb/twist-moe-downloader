5c95d22039cf2150fcd28e67afa5ffbc94b75320
James Fotherby <fotherby1@gmail.com>
2019-12-17 14:39:47 -0800

    Add search function, include range by default

--- README.md
+++ README.md
@@ -25,11 +25,15 @@ and you're done!!
 ### Running the script
 
 ```bash
-$ python twist-dl.py 
+$ python twist-dl.py
+usage: python twist-dl.py search [title] / [title] [--directory DIRECTORY] [-h]
+
 Twist-dl is a small python tool for downloading video contents of series
 available on the website twist.moe locally! To download a particular series,
 find the title as defined in twist.moe's url string. i.e. 'https://twist.moe/a
-/made-in-abyss/' would need the argument passed as 'made-in-abyss'.
+/made-in-abyss/' would need the argument passed as 'made-in-abyss'. Optionally
+searching can be enabled for searching through twist.moe's catalog by using
+the search command.
 
 positional arguments:
   title
@@ -38,18 +42,40 @@ optional arguments:
   -h, --help            show this help message and exit
   --directory DIRECTORY
                         Directory path to save downloaded contents
+  --range RANGE         Range of episodes to download. i.e. --range=1-24
 
 ```
 
-##### Example Usage:
+### Example Usage:
+
+#### Search
+Use the search command to retrieve the title name for downloading:
+```bash
+$ python twist-dl.py search shingeki-no-kyojin
+title                              | created_at          | updated_at          | id
+-----------------------------------+---------------------+---------------------+-----
+shingeki-no-kyojin                 | 2016-08-14 12:50:06 | 2016-08-14 12:50:06 | 657
+-----------------------------------+---------------------+---------------------+-----
+shingeki-no-kyojin-season-2        | 2017-04-01 20:59:42 | 2017-04-01 20:59:42 | 1019
+-----------------------------------+---------------------+---------------------+-----
+shingeki-no-kyojin-season-3        | 2018-07-22 23:20:00 | 2018-07-22 23:20:00 | 1508
+-----------------------------------+---------------------+---------------------+-----
+shingeki-no-kyojin-ova             | 2019-08-02 18:10:15 | 2019-08-02 18:10:15 | 2078
+-----------------------------------+---------------------+---------------------+-----
+shingeki-no-kyojin-lost-girls      | 2019-08-02 18:13:33 | 2019-08-02 18:13:33 | 2079
+-----------------------------------+---------------------+---------------------+-----
+shingeki-no-kyojin-kuinaki-sentaku | 2019-08-02 18:15:58 | 2019-08-02 18:15:58 | 2080
+```
+
+#### Download
+Download by passing the series title as found from search. Not specifying a range will download the entire contents of the series.
 ```bash
 $ python twist-dl.py shingeki-no-kyojin
+https://twist.moe/api/anime/shingeki-no-kyojin/sources
 Successfully gathered series information.
-Beginning Episode Range:1
-Ending Episode Range:2
-Downloading MP4s to Path: /Users/jfotherby/Desktop/projects/twist.moe-dl/anime/shingeki-no-kyojin/.
-/Users/jfotherby/Desktop/projects/twist.moe-dl/anime/shingeki-no-kyojin/shingeki-no-kyojin-episode-1.mp4:   7%|▋         | 45351/662939 [00:04<00:49, 12597.16KB/s]
-```
+Downloading MP4s to Path: anime/shingeki-no-kyojin/ with episode range of 1-25
+anime/shingeki-no-kyojin/shingeki-no-kyojin-episode-1.mp4:   4%|▎         | 24479/662939 [00:03<01:02, 10219.80KB/s]
 
+```
 ##### Disclaimer
 Downloading copyright videos may be illegal in your country. This tool is for educational purposes only.
--- twist-dl.py
+++ twist-dl.py
@@ -35,19 +35,58 @@ def validate_episode_input(s):
     except Exception:
         exit(1)
 
+def basic_search(query, response):
+    search_results = []
+    for title in response:
+        name = title['slug']['slug']
+        if query.lower() in name.lower():
+            title['slug']['title'] = name
+            del title['slug']['slug']
+            del title['slug']['anime_id']
+            search_results.append(title['slug'])
+    return search_results
+
+def printTable(myDict, colList=None, sep='  '):
+    """ Pretty print a list of dictionaries (myDict) as a dynamically sized table.
+    Author: Thierry Husson
+    """
+    if not colList: colList = list(myDict[0].keys() if myDict else [])
+    myList = [colList] # 1st row = header
+    for item in myDict: myList.append([str(item[col] or '') for col in colList])
+    colSize = [max(map(len,(sep.join(col)).split(sep))) for col in zip(*myList)]
+    formatStr = ' | '.join(["{{:<{}}}".format(i) for i in colSize])
+    line = formatStr.replace(' | ','-+-').format(*['-' * i for i in colSize])
+    item=myList.pop(0); lineDone=False
+    while myList:
+        if all(not i for i in item):
+            item=myList.pop(0)
+            if line and (sep!='\uFFFA' or not lineDone): print(line); lineDone=True
+        row = [i.split(sep,1) for i in item]
+        item = [i[1] if len(i)>1 else '' for i in row]
+        print(formatStr.format(*[i[0] for i in row]))
+
+
 if __name__ == '__main__':
     base_dir = os.path.dirname(os.path.realpath(__file__))
-    usage = "python twist-dl.py title [--directory DIRECTORY] [-h]"
+    usage = "python twist-dl.py search [title] / [title] [--directory DIRECTORY] [-h]"
     parser = ArgumentParser(
         description="Twist-dl is a small python tool for downloading video contents of series available on the website "
                     "twist.moe locally! To download a particular series, find the title as defined in twist.moe's url string."
-                    " i.e. 'https://twist.moe/a/made-in-abyss/' would need the argument passed as 'made-in-abyss'.",
+                    " i.e. 'https://twist.moe/a/made-in-abyss/' would need the argument passed as 'made-in-abyss'. Optionally"
+                    " searching can be enabled for searching through twist.moe's catalog by using the search command.",
         usage=usage
     )
-    parser.add_argument('title')
+    parser.add_argument('title', nargs='+')
+
 
     parser.add_argument("--directory", dest="directory",
                         help="Directory path to save downloaded contents",
+                        required=False,
+                        default=""
+                        )
+
+    parser.add_argument("--range", dest="range",
+                        help="Range of episodes to download. i.e. --range=1-24",
                         required=False,
                         default=""
                         )
@@ -56,54 +95,72 @@ if __name__ == '__main__':
         parser.print_help()
         exit(1)
 
+    # Parse arg vars.
     options = parser.parse_args()
     title = options.title
+    episode_range = options.range
+
+    # Configuration variables.
+    with open('{}/conf.json'.format(base_dir)) as f:
+        configuration = json.loads(f.read())
+    base_url = configuration['base_url']
+    api_token = configuration['api_token']
+    source_key = str(configuration['source_key'])
+    # API Auth Header
+    headers = {'x-access-token': '{}'.format(api_token)}
+
+    # Search option
+    if title[0].lower() == 'search':
+        headers = {'x-access-token': '{}'.format(api_token)}
+        r = requests.get(base_url + '/api/anime', headers=headers)
+        r = json.loads(r.content)
+        printTable(basic_search(query=title[1], response=r))
+        exit(0)
+    else:
+        title = title[0]
+
+    # Directory Path Handling.
     if not options.directory:
-        if not os.path.isdir('{}/anime'.format(base_dir)):
-            os.mkdir('{}/anime'.format(base_dir))
-        directory = '{}/anime/{}/'.format(base_dir, title)
+        if not os.path.isdir('anime'):
+            os.mkdir('anime')
+        directory = 'anime/{}/'.format(title)
     else:
         directory = options.directory
         if not directory.endswith('/'):
             directory += '/'
 
-    with open('{}/conf.json'.format(base_dir)) as f:
-        configuration = json.loads(f.read())
-    base_url = configuration['base_url']
-    api_url = '{}/api/anime/{}/sources'.format(base_url, title)
-    api_token = configuration['api_token']
-    source_key = str(configuration['source_key'])
-
-    # Get series data, which includes episode data and encrypted sourcefile paths
-    headers = {'x-access-token': '{}'.format(api_token)}
-    r = requests.get(api_url, headers=headers)
+    # Get series data, which includes episode data and encrypted sourcefile paths.
+    url = '{}/api/anime/{}/sources'.format(base_url, title)
+    r = requests.get(url, headers=headers)
+    print url
     if r.status_code == 404:
         print "Error, it looks like the title you entered is not found, " \
               "please ensure it is identical to the url path listed in twist.moe"
         exit(1)
     series_data = json.loads(r.content)
     print('Successfully gathered series information.')
-
-    episode_range_begin = validate_episode_input(raw_input('Beginning Episode Range:'))
-    episode_range_end = validate_episode_input(raw_input('Ending Episode Range:'))
-
-    # Decrypt the source url and get a list of source URLs
+    # Decrypt the source url and get a list of source URLs.
     source_url_list = get_series_url_list(series_data)
 
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
 
-    # Download files to user specified path
+    # Download entire range if range not specified.
+    if episode_range:
+        episode_begin = validate_episode_input(episode_range.split('-')[0])
+        episode_end = validate_episode_input(episode_range.split('-')[1])
+    else:
+        episode_begin = 1
+        episode_end = len(source_url_list[title])
+
+    print 'Downloading MP4s to Path: {} with episode range of {}-{}'.format(directory, episode_begin, episode_end)
     headers = {
         'User-Agent': \
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 '
             '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
         'Referer': 'https://twist.moe/'
     }
-
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
-
-    print 'Downloading MP4s to Path: {}.'.format(directory)
-    for episode in range(episode_range_begin, episode_range_end + 1):
+    for episode in range(episode_begin, episode_end + 1):
         path = '{}{}-episode-{}.mp4'.format(directory, title, episode)
         url = source_url_list[title][episode]
         download_file(url=url, headers=headers, path=path)
